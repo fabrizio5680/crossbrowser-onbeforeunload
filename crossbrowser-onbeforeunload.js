ca935bc7a522189b6f09da58927247f52c3cc421
Fabrizio Bottaro <fabrizio5680@gmail.com>
2015-11-02 16:31:30 +0000

    initial commit

--- crossbrowser-onbeforeunload.js
+++ crossbrowser-onbeforeunload.js
@@ -1,36 +1,9 @@
 (function (window, document) {
-	angular.module('crossbrowser-onbeforeunload', [])
 
-		.service('refreshPrevent', function () {
+	var message = "Are you sure ?";
 
-			var message = "Are you sure ?";
-
-			var onbeforeunload = function (e) {
-
-				var firefox = /Firefox[\/\s](\d+)/.test(navigator.userAgent);
-				if (firefox) {
-					//Add custom dialog
-					//Firefox does not accept window.showModalDialog(), window.alert(), window.confirm(), and window.prompt() furthermore
-					var dialog = document.createElement("div");
-					document.body.appendChild(dialog);
-					dialog.id = "dialog";
-					dialog.style.visibility = "hidden";
-					dialog.innerHTML = message;
-					var left = document.body.clientWidth / 2 - dialog.clientWidth / 2;
-					dialog.style.left = left + "px";
-					dialog.style.visibility = "visible";
-					var shadow = document.createElement("div");
-					document.body.appendChild(shadow);
-					shadow.id = "shadow";
-					//tip with setTimeout
-					setTimeout(function () {
-						document.body.removeChild(document.getElementById("dialog"));
-						document.body.removeChild(document.getElementById("shadow"));
-					}, 0);
-				}
-				return message;
-			};
-
+	var mod = angular.module('crossbrowser-onbeforeunload', [])
+	mod.service('refreshPrevent', function () {
 
 			this.bind = function (customMsg) {
 
@@ -51,4 +24,33 @@
 				}
 			}
 		});
+
+
+	function onbeforeunload (e) {
+
+		var firefox = /Firefox[\/\s](\d+)/.test(navigator.userAgent);
+		if (firefox) {
+			//Add custom dialog
+			//Firefox does not accept window.showModalDialog(), window.alert(), window.confirm(), and window.prompt() furthermore
+			var dialog = document.createElement("div");
+			document.body.appendChild(dialog);
+			dialog.id = "dialog";
+			dialog.style.visibility = "hidden";
+			dialog.innerHTML = message;
+			var left = document.body.clientWidth / 2 - dialog.clientWidth / 2;
+			dialog.style.left = left + "px";
+			dialog.style.visibility = "visible";
+			var shadow = document.createElement("div");
+			document.body.appendChild(shadow);
+			shadow.id = "shadow";
+			//tip with setTimeout
+			setTimeout(function () {
+				document.body.removeChild(document.getElementById("dialog"));
+				document.body.removeChild(document.getElementById("shadow"));
+			}, 0);
+		}
+		return message;
+	}
+
+
 }(window, document));
